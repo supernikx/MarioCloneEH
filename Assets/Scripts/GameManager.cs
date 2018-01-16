6f5037641a0939d11979f9031ed875f44ca755f3
MI_C_06\TamagnaDavide <supernikx@gmail.com>
2018-01-16 12:54:30 +0100

    aggiunto int marco

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour {
     public GameObject pauseScreen;
     public GameObject optionsMenu, pauseMenu;
     private bool isPaused;
+    private int Marcoculo;
 
     private void Start()
     {
