b98c1c796f3ea30ab475f7b9bdf4482885dfd354
TamagnaDavide <supernikx@gmail.com>
2018-01-17 18:12:07 +0100

    aggiunto timer che disabilita il potere

--- Assets/Scripts/FlowerUpScript.cs
+++ Assets/Scripts/FlowerUpScript.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlowerUpScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    public int powerUpDuration;
+    private Player p;
+    private Renderer rend;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "player")
         {
-            Player p = other.gameObject.GetComponent<Player>();
+            p = other.gameObject.GetComponent<Player>();
             p.isFlower = true;
-            gameObject.SetActive(false);
-
-            
+            StartCoroutine(DisablePowerUp());
+            rend.enabled = false;
         }
     }
 
+    private IEnumerator DisablePowerUp()
+    {
+        yield return new WaitForSeconds(seconds: powerUpDuration);
+        p.isFlower = false;
+        gameObject.SetActive(false);
+    }
 }
